9c3caaf02ce8da349a9e2b8692e6c24b5c6bbb2d
James Folberth <jamesfolberth@gmail.com>
2014-04-05 10:47:02 -0600

    added some documentation stuff

---
+++ main.m
@@ -0,0 +1,30 @@
+function [] = main()
+% main - Driver routine to test signal recovery algorithm.
+%        Currently used for development of the code.
+%
+% Syntax: 
+%  [] = main()
+%
+% Inputs:
+%  None
+%
+% Outputs:
+%  None
+%
+% Example:
+%  main()
+%
+% Dependencies:
+% 
+% TODO:
+%  Pretty much everything... ;)
+%
+% Authors: JF,EY
+% Revision history:
+%  05 April 2014 - Initial writing 
+
+% For repeatability, set default PRNG (Mersenne twister) and seed (seed = 0)
+rng('default');
+
+
+end 
